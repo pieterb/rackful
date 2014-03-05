5eae24dfad8626b4378fadd5e36752d2d5f6d850
Pieter van Beek <pieter@djinnit.com>
2014-03-05 18:50:37 +0100

    import and export scripts

---
+++ import.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This script is supposed to be run before each git commit.
+
+# Establish the top level repository directory:
+REPODIR=$( dirname "${BASH_SOURCE[0]}" )
+
+# Echo everything we do to export.log:
+set -x
+{
+
+### Step 1: General ###
+cd "${REPODIR}"
+
+
+} 2>"export.log"
