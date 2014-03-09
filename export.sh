bb28b63ed6a61a2bfa1d286f38901c8666cee823
Pieter van Beek <pieter@djinnit.com>
2014-03-09 23:03:32 +0100

    Better export.sh

--- export.sh
+++ export.sh
@@ -5,24 +5,30 @@
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 REPODIR="$PWD"
 
+function export_recursively() {
+  [ -n "$1" ] && pushd "$1" &>/dev/null
+  [ -d '.git' ] && [ -n "$1" ] || {
+    # This code is run only for non-subrepositories:
+    find . -type d -maxdepth 1 -mindepth 1 | while read i; do
+      [ './.git' = "$i" ] || export_recursively "$i"
+    done
+    find . -name "*.${USER}" -maxdepth 1 | while read i; do
+      if ! [ -e "${i%\.${USER}}" ]; then
+        echo -n "Creating symlink for '${i}'..."
+        ln -s "`basename "${i}"`" "${i%\.${USER}}" && echo ' OK' || echo ' FAILED'
+      fi
+    done
+  }
+  [ -n "$1" ] && {
+    [ -x 'export.sh' ] && './export.sh'
+    popd &>/dev/null
+  }
+}
+
 # Echo everything we do to export.log:
 set -x
 {
-
-### Step 1: General ###
-
-# Some files (.rvmrc and .project for example) may differ per developer.
-# In the git repo, these files are called <file_name>.<developer_name>.
-# For example: ".rvmrc.pieterb"
-# The following lines put such files in place:
-find . -name "*.${USER}" | while read i; do
-    if ! [ -e "${i%\.${USER}}" ]; then
-        echo -n "Creating symlink for '${i}'..."
-        ln -s "`basename "${i}"`" "${i%\.${USER}}" && echo ' OK' || echo ' FAILED'
-    fi
-done
-
-[ -h rack ] && rm rack
-[ -e rack ] || ln -s "${GEM_HOME}/gems/rack-"[0-9]* rack
-
-} 2>"export.log"
+  export_recursively
+  [ -h rack ] && rm rack
+  [ -e rack ] || ln -s "${GEM_HOME}/gems/rack-"[0-9]* rack
+} 2>'export.log'
