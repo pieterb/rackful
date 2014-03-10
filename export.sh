71e5d570d405577c46f0e001013363cfba60d162
Pieter van Beek <pieter@djinnit.com>
2014-03-10 15:54:43 +0100

    better export.sh

--- export.sh
+++ export.sh
@@ -5,30 +5,32 @@
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 REPODIR="$PWD"
 
-function export_recursively() {
-  [ -n "$1" ] && pushd "$1" &>/dev/null
-  [ -d '.git' ] && [ -n "$1" ] || {
-    # This code is run only for non-subrepositories:
-    find . -type d -maxdepth 1 -mindepth 1 | while read i; do
-      [ './.git' = "$i" ] || export_recursively "$i"
-    done
-    find . -name "*.${USER}" -maxdepth 1 | while read i; do
-      if ! [ -e "${i%\.${USER}}" ]; then
-        echo -n "Creating symlink for '${i}'..."
-        ln -s "`basename "${i}"`" "${i%\.${USER}}" && echo ' OK' || echo ' FAILED'
-      fi
-    done
-  }
-  [ -n "$1" ] && {
-    [ -x 'export.sh' ] && './export.sh'
-    popd &>/dev/null
-  }
-}
+GIT_REPOSITORIES="$(
+  find . -name .git -mindepth 2 | while read i; do
+    echo -n " -not -path '$(dirname "$i")/*'"
+  done
+)"
 
-# Echo everything we do to export.log:
-set -x
-{
-  export_recursively
-  [ -h rack ] && rm rack
-  [ -e rack ] || ln -s "${GEM_HOME}/gems/rack-"[0-9]* rack
-} 2>'export.log'
+# Link to user-specific files:
+find . -name "*.${USER}" $GIT_REPOSITORIES -not -path './client/data/*' |
+while read i; do
+  if ! [ -e "${i%\.${USER}}" ]; then
+    echo -n "Creating symlink for '${i}'..."
+    ln -s "`basename "${i}"`" "${i%\.${USER}}" && echo ' OK' || echo ' FAILED'
+  else
+    [ -h "${i%\.${USER}}" ] || echo "WARNING: ${i%\.${USER}} is not a symlink!"
+  fi
+done
+
+# Run export.sh scripts in subdirectories:
+eval "find . -name 'export.sh' -mindepth 2 $GIT_REPOSITORIES" |
+while read i; do
+  pushd "$(dirname "$i")" &>/dev/null
+    echo "Running ${i}..."
+    ./export.sh
+    echo "... Done"
+  popd &>/dev/null
+done
+
+[ -h rack ] && rm rack
+[ -e rack ] || ln -s "${GEM_HOME}/gems/rack-"[0-9]* rack
